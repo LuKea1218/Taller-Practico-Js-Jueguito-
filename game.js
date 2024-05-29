68d97cf856487e50070abbebfa8c88e3f8db0881
LuKea <lucassesa1218@gmail.com>
2024-05-29 15:11:15 -0500

    Clase 3 🥵🗿

--- game.js
+++ game.js
@@ -1,8 +1,28 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener("load", startGame);
+window.addEventListener('load', startGame);
 
 function startGame() {
-    game.fillRect(0,0,100,100)
+  let canvasSize;
+
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+  
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+  
+  const elementsSize = canvasSize / 10;
+
+  console.log({ canvasSize, elementsSize });
+
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = 'end';
+
+  for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis['X'], elementsSize, elementsSize * i);
+  }
 }
